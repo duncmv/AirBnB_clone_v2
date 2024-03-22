ce79e16d4a5e9608b93853d717358f34c97cdce1
duncmv <dtindiwensi@gmail.com>
2024-03-22 13:01:08 +0300

    mandatory

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,6 +14,13 @@ def states():
     return render_template('7-states_list.html', states=states)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities():
+    """retrieve states"""
+    states = storage.all('State')
+    return render_template('8-cities_by_states.html', states=states)
+
+
 @app.teardown_appcontext
 def teardown(err):
     """close session"""
