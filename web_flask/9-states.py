30cf6dd58cbd856f78032c9d8dca5c72ce8609aa
duncmv <dtindiwensi@gmail.com>
2024-03-22 13:22:19 +0300

    mandatory

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from models import *
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_given_id(id):
+    states = storage.all("State")
+    found_state = None
+    for s_id in states:
+        if id in s_id:
+            found_state = states[s_id]
+
+    return render_template('9-states.html',
+                           state=found_state)
+
+
+@app.teardown_appcontext
+def teardown(err):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
