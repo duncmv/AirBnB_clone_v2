9506fead71e5cdbc1c207b6cf6646520c3b4bd08
duncmv <dtindiwensi@gmail.com>
2024-03-23 13:51:47 +0300

    advanced

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""creates flask app to handle states an cities"""
+from models import *
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """retrieves data for page"""
+    states = storage.all("State")
+    amenities = storage.all("Amenity").values()
+    places = storage.all("Place").values()
+    users = storage.all("User").values()
+    return render_template('100-hbnb.html', states=states, 
+                           amenities=amenities, places=places,
+                           users=users)
+
+
+@app.teardown_appcontext
+def teardown(err):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
